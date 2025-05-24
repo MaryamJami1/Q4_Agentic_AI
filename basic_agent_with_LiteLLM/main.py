bbc84340a2fe19a32e177dfe04828ad16b396c9b
MaryamJami1 <maryamchouhan373@gmail.com>
2025-05-25 02:03:35 +0500

    agent with litellm'

---
+++ basic_agent_with_LiteLLM/main.py
@@ -0,0 +1,32 @@
+# step 1: required imports
+import os
+from dotenv import load_dotenv
+from litellm import completion, exceptions
+
+load_dotenv()
+
+# set environment for LiteLLM
+os.environ["LITELLM_API_BASE"] = "https://openrouter.ai/api/v1"
+key = os.environ["LITELLM_API_KEY"] = os.getenv("OPENROUTER_API_KEY")
+
+
+# step 2: user input
+query = input("You: ")
+
+# step 3: send to Gemini via OpenRouter
+try:
+    print("\n🤖 AI is replying...")
+    response = completion(
+        model="openrouter/mistralai/mistral-7b-instruct",  # ✅ valid model name for OpenRouter
+        messages=[{"role": "user", "content": query}]
+    )
+    print("Mistral:", response['choices'][0]['message']['content'])
+
+# step 4: fallback to GPT-3.5 via OpenRouter
+except exceptions.BadRequestError as e:
+    print("\n❌ Mistral failed, switching to Meta.")
+    response = completion(
+        model="openrouter/meta-llama/llama-3-8b-instruct",  # ✅ OpenRouter model name
+        messages=[{"role": "user", "content": query}]
+    )
+    print("llama-3-8b:", response['choices'][0]['message']['content'])
